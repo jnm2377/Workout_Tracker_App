ac88f9410cc737875e256446173d8be28b8f1d5a
Josefina Mancilla <josefinanoemi4@gmail.com>
2017-12-02 22:48:14 -0600

    Server.js file set up, and dependecies installed/required

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const morgan = require('morgan');
+const session = require('express-session');
+const methodOverride = require('method-override');
+const bcrypt = require('bcrypt');
+const app = express();
+require('pretty-error').start();
+const port = 3000;
+
+//require controllers
+
+//db
+
+//fail/success
+
+//middleware
+
+//listener
+
+app.listen(port, () => {
+  console.log(================);
+  console.log("Mongoose app running: ",);
+  console.log(================);
+})
